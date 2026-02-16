49b7711bcbad08caf3a802f0c88a92ffa47738b9
Vauzi-17 <vauzitriutomo5.com>
2026-02-16 11:09:30 +0700

    change

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,18 +3,26 @@ export default function sitemap() {
     {
       url: "https://vauzi-portfolio.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
       url: "https://vauzi-portfolio.vercel.app/about",
       lastModified: new Date(),
-    },
-    {
-      url: "https://vauzi-portfolio.vercel.app/contact",
-      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
     },
     {
       url: "https://vauzi-portfolio.vercel.app/projects",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://vauzi-portfolio.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
     },
   ];
 }
